25d9535b286e5e3db46e7f772c4deb2ad24dfd33
jellythewobbly <jellythewobbly@hotmail.com>
2018-09-10 22:32:52 +0800

    Converted to ES5, works on Chrome, Firefox, Safari, IE10

--- scriptES6.js
+++ scriptES6.js
@@ -1,20 +1,10 @@
-'use strict';
-
 // Shorten DOM actions
-var elementId = function(id) {
-	return document.getElementById(id);
-};
-var elementsClass = function(className) {
-	return document.getElementsByClassName(className);
-};
-var appendMain = function(element) {
-	return elementId('main').appendChild(element);
-};
-var create = function(element) {
-	return document.createElement(element);
-};
+const elementId = id => document.getElementById(id);
+const elementsClass = className => document.getElementsByClassName(className);
+const appendMain = element => elementId('main').appendChild(element);
+const create = element => document.createElement(element);
 
-var colors = {
+const colors = {
 	white: '#ffffff',
 	red: '#ff0000',
 	lime: '#00ff00',
@@ -30,38 +20,41 @@ var colors = {
 // autosave reset upon refresh/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', function() {
 	if (localStorage.getItem('savedState')) {
-		JSON.parse(localStorage.getItem('savedState')).forEach(function(i) {
-			createNote(i.title, i.content, i.color);
-		});
+		JSON.parse(localStorage.getItem('savedState')).forEach(i =>
+			createNote(i.title, i.content, i.color)
+		);
 	}
 });
 
-var Note = function(title = '', content = '', color = 'white') {
-	this.title = title;
-	this.content = content;
-	this.color = color;
-};
+class Note {
+	constructor(title = '', content = '', color = 'white') {
+		this.title = title;
+		this.content = content;
+		this.color = color;
+	}
+}
 
-var holder = [];
+let holder = [];
 
 // creating note by class + appending to getElementById('main')
-var createNote = function(t, cont, col) {
-	var test = new Note(t, cont, col);
+const createNote = (t, cont, col) => {
+	const test = new Note(t, cont, col);
 
 	// title of note
-	var title = create('input');
+	let title = create('input');
 	t ? (title.defaultValue = t) : (title.placeholder = 'Title');
 	title.className = 'title';
 	title.addEventListener('focusout', function() {
 		test.title = title.value;
 		// save to localStorage?
-		setTimeout(function() {
-			localStorage.setItem('savedState', JSON.stringify(holder));
-		}, 0);
+		setTimeout(
+			() => localStorage.setItem('savedState', JSON.stringify(holder)),
+			0
+		);
 	});
 
 	// text area for note body
-	var content = create('textarea');
+	let content = create('textarea');
 	cont
 		? (content.textContent = cont)
 		: (content.placeholder = 'Take a note...');
@@ -70,48 +63,49 @@ var createNote = function(t, cont, col) {
 	content.addEventListener('focusout', function() {
 		test.content = content.value;
 		// save to localStorage?
-		setTimeout(function() {
-			localStorage.setItem('savedState', JSON.stringify(holder));
-		}, 0);
+		setTimeout(
+			() => localStorage.setItem('savedState', JSON.stringify(holder)),
+			0
+		);
 	});
 
 	// dropdown select for note color
-	var select = create('select');
+	let select = create('select');
 	select.className = 'selectColor';
-	for (var color in colors) {
-		var option = create('option');
+	for (let color in colors) {
+		let option = create('option');
 		option.value = color;
 		option.textContent = color.replace('_', ' ');
 		select.appendChild(option);
 	}
-	var index = Array.from(select).findIndex(function(i) {
-		return i.value == col;
-	});
+	const index = Array.from(select).findIndex(i => i.value == col);
 	select.selectedIndex = col ? index : 0;
 	select.addEventListener('change', function() {
 		this.parentElement.style.backgroundColor = colors[this.value];
 		test.color = this.value;
 		// save to localStorage?
-		setTimeout(function() {
-			localStorage.setItem('savedState', JSON.stringify(holder));
-		}, 0);
+		setTimeout(
+			() => localStorage.setItem('savedState', JSON.stringify(holder)),
+			0
+		);
 	});
 
 	// delete button within note
-	var delButton = create('button');
+	let delButton = create('button');
 	delButton.className = 'delButton';
 	delButton.textContent = 'Delete';
 	delButton.addEventListener('click', function() {
 		this.parentElement.parentNode.removeChild(note);
 		holder.splice(holder.indexOf(test), 1);
 		// save to localStorage?
-		setTimeout(function() {
-			localStorage.setItem('savedState', JSON.stringify(holder));
-		}, 0);
+		setTimeout(
+			() => localStorage.setItem('savedState', JSON.stringify(holder)),
+			0
+		);
 	});
 
 	// creating note itself and appending elements
-	var note = create('div');
+	let note = create('div');
 	note.appendChild(title);
 	note.appendChild(content);
 	note.appendChild(select);
@@ -128,16 +122,17 @@ var createNote = function(t, cont, col) {
 		this.querySelector('select').style.visibility = 'hidden';
 		this.querySelector('button').style.visibility = 'hidden';
 		// save to localStorage?
-		setTimeout(function() {
-			localStorage.setItem('savedState', JSON.stringify(holder));
-		}, 0);
+		setTimeout(
+			() => localStorage.setItem('savedState', JSON.stringify(holder)),
+			0
+		);
 	});
 
 	appendMain(note);
 	holder.push(test);
 };
 
-var deleteAll = function() {
+const deleteAll = () => {
 	if (window.confirm('ARE YOU SURE?')) {
 		// reset actions
 		holder = [];
@@ -148,13 +143,17 @@ var deleteAll = function() {
 	}
 };
 
-var search = function() {
+const search = () => {
 	// searching in both title and content by using array.filter
-	var searchResults = Array.from(elementsClass('note')).filter(function(i) {
-		return i.children[0].value
-			.toLowerCase()
-			.includes(elementId('search').value.toLowerCase());
-	});
+	const searchResults = Array.from(elementsClass('note')).filter(
+		i =>
+			i.children[0].value
+				.toLowerCase()
+				.includes(elementId('search').value.toLowerCase()) ||
+			i.children[1].value
+				.toLowerCase()
+				.includes(elementId('search').value.toLowerCase())
+	);
 	// reset
 	while (elementId('main').firstChild) {
 		elementId('main').removeChild(elementId('main').firstChild);
@@ -162,15 +161,13 @@ var search = function() {
 	// change title to 'SEARCH RESULTS'
 	elementId('heading').textContent = 'SEARCH RESULTS';
 	// append results
-	searchResults.forEach(function(i) {
-		return appendMain(i.cloneNode(true));
-	});
+	searchResults.forEach(i => appendMain(i.cloneNode(true)));
 	// reset search form
 	elementId('search').value = '';
 	elementId('back').style.visibility = 'visible';
 };
 
-var back = function() {
+const back = () => {
 	// hide back button
 	elementId('back').style.visibility = 'hidden';
 	holder = [];
@@ -179,7 +176,7 @@ var back = function() {
 	}
 	// change title to 'STICKY NOTES'
 	elementId('heading').textContent = 'STICKY NOTES';
-	JSON.parse(localStorage.getItem('savedState')).forEach(function(i) {
-		return createNote(i.title, i.content, i.color);
-	});
+	JSON.parse(localStorage.getItem('savedState')).forEach(i =>
+		createNote(i.title, i.content, i.color)
+	);
 };
